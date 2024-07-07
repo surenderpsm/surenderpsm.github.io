634762e53d0c62156705bdefc982401c809ecbcb
Surender Soundiramourty <psm.surender@gmail.com>
2024-07-07 22:12:59 +0800

    updated dm toggle position

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   darkMode: 'selector',
-  content: ["./public/*.{html,js}"],
+  content: ["./*.{html,js}"],
   theme: {
     extend: {
       keyframes:{
