e1ace0e32b981a45030b898ae5200d643ce5d9b3
Surender Soundiramourty <psm.surender@gmail.com>
2024-07-09 23:19:46 +0800

    design changes

--- src/pages/index.js
+++ src/pages/index.js
@@ -53,9 +53,8 @@ const IndexPage = () => {
     <>
       
       <DarkModeToggle/>
-      <header className="h-screen md:h-fit">
+      <header className="h-screen sm:h-fit flex flex-col justify-center">
           <div className="flex flex-col items-center m-10">
-              {/* <img className="animate-[fadeInBottom_1s_ease-in-out] size-[200px] rounded-full" src={dp} alt="A headshot of Surender with foliage in the background"/> */}
               <StaticImage src="../images/dp.jpg" className="animate-[fadeInBottom_1s_ease-in-out] size-[200px] rounded-full" alt="A headshot of Surender with foliage in the background"/>
               <div className="animate-[fadeInBottom_1.5s_ease-in-out] font-display font-extralight text-center text-4xl mt-10 mb-2">
                   Hi, I am <span className="font-extrabold text-5xl">Surender</span>
@@ -64,6 +63,7 @@ const IndexPage = () => {
                   Computer Science Graduate | Front-end Developer | Data Science Enthusiast
               </div>
           </div>
+          <svg className="m-20 md:hidden animate-pulse" viewBox="-2.4 -2.4 28.80 28.80" fill="none" title="Scroll Down"><g id="SVGRepo_bgCarrier" strokeWidth="0"></g><g id="SVGRepo_tracerCarrier" strokeLinecap="round" strokeLinejoin="round"></g><g id="SVGRepo_iconCarrier"> <path d="M7 10L12 15L17 10" stroke="currentColor" strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round"></path> </g></svg>
       </header>
 
       <main>
